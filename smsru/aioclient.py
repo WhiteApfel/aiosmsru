5b447280e290c6ed9ffedc862f8b815b73943e49
Eugene Mayer <white@pfel.ru>
2021-10-25 06:27:32 +0300

    ✨ Add basic request method

--- smsru/aioclient.py
+++ smsru/aioclient.py
@@ -3,8 +3,9 @@ from httpx import AsyncClient
 
 
 class AioSMSru:
-    def __init__(self, api_id: str):
+    def __init__(self, api_id: str, domain: str = "sms.ru"):
         self._api_id = api_id
+        self._domain = domain
         self._client: Optional[AsyncClient] = None
 
     @property
@@ -12,3 +13,8 @@ class AioSMSru:
         if not self._client:
             self._client = AsyncClient()
         return self._client
+
+    async def _request(self, endpoint: str, **params):
+        params.update({"api_id": self._api_id, "json": 1})
+        response = await self.client.get(f"https://{self._domain}/{endpoint}", params=params)
+        return response
--- smsru/client.py
+++ smsru/client.py
@@ -3,8 +3,9 @@ from httpx import Client
 
 
 class SMSru:
-    def __init__(self, api_id: str):
+    def __init__(self, api_id: str, domain: str = "sms.ru"):
         self._api_id = api_id
+        self._domain = domain
         self._client: Optional[Client] = None
 
     @property
@@ -12,3 +13,8 @@ class SMSru:
         if not self._client:
             self._client = Client()
         return self._client
+
+    def _request(self, endpoint: str, **params):
+        params.update({"api_id": self._api_id, "json": 1})
+        response = self.client.get(f"https://{self._domain}/{endpoint}", params=params)
+        return response
