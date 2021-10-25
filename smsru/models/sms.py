8694a0546df4e8f1a6ef38666c7b227ef014ba77
Eugene Mayer <white@pfel.ru>
2021-10-25 11:45:56 +0300

    🏷️ Add check_sms models

--- smsru/models/sms.py
+++ smsru/models/sms.py
@@ -9,3 +9,11 @@ class SMSruSendSms(SMSruBase):
 
 class SMSruSendSmsResponse(SMSruWithBalance):
     sms: Dict[str, SMSruSendSms]
+
+
+class SMSruCheckSms(SMSruBase):
+    cost: Optional[float]
+
+
+class SMSruCheckSmsResponse(SMSruWithBalance):
+    sms: Dict[str, SMSruCheckSms]
