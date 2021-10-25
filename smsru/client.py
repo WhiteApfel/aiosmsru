4abe95406be2e0b05e3ffeef09773b8d3c5d2ee9
Eugene Mayer <white@pfel.ru>
2021-10-25 12:13:36 +0300

    ✨ Add sms_cost methods

--- smsru/client.py
+++ smsru/client.py
@@ -7,7 +7,11 @@ else:
     from typing import Sequence
 from httpx import Client, Response
 
-from smsru.models.sms import SMSruSendSmsResponse, SMSruCheckSmsResponse
+from smsru.models.sms import (
+    SMSruSendSmsResponse,
+    SMSruCheckSmsResponse,
+    SMSruSmsCostResponse,
+)
 
 
 class SMSru:
@@ -99,3 +103,64 @@ class SMSru:
         response = self._request("sms/status", params)
 
         return SMSruCheckSmsResponse(**response.json())
+
+    def sms_cost(
+        self, recipients: Union[str, Sequence[str]], messages: Union[str, Sequence[str]]
+    ):
+        params = {}
+        if (  # More than one recipient
+            isinstance(recipients, Sequence)
+            and not isinstance(recipients, str)
+            and len(recipients) > 1
+        ):
+            if (  # More than one message
+                isinstance(messages, Sequence)
+                and not isinstance(messages, str)
+                and len(messages) > 1
+            ):
+                if len(recipients) == len(messages):
+                    for i, recipient in enumerate(recipients):
+                        params[f"to[{recipient}]"] = messages[i]
+                else:
+                    raise ValueError(
+                        "Sequences of recipients and messages must be of the same length"
+                    )
+            elif (  # One message for all recipients
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = ",".join(recipients)
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "Messages must be str (message) or sequence of str (messages)"
+                )
+        elif (  # Only one recipient
+            isinstance(recipients, str)
+            or isinstance(recipients, Sequence)
+            and len(recipients) == 1
+        ):
+            if (
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = recipients
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "If there is only one recipient, then there can be no more than one message"
+                )
+        else:
+            raise ValueError(
+                "Recipients must be str (phone number) or sequence of str (phone numbers)"
+            )
+
+        response = self._request("sms/cost", params)
+
+        return SMSruSmsCostResponse(**response.json())
