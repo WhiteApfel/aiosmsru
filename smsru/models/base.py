c8d92898e7adc9455528901c3f1161d714bd43ec
Eugene Mayer <white@pfel.ru>
2021-10-25 06:57:07 +0300

    🏷️ Add optional `status_text` on error response

--- smsru/models/base.py
+++ smsru/models/base.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class SMSruBase(BaseModel):
     status: str
     status_code: int
+    status_text: Optional[str]
 
 
 class SMSruWithBalance(SMSruBase):
