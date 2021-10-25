44bc21384cde70707e5fea4ff88203c1570138ec
Eugene Mayer <white@pfel.ru>
2021-10-25 08:35:44 +0300

    ✅ Add two recipients two messages test

--- tests/test_send_sms.py
+++ tests/test_send_sms.py
@@ -82,3 +82,33 @@ async def test_aio_send_any_sms_one_text(httpx_mock: HTTPXMock):
     assert response.sms['79991398805'].sms_id == "000000-10000000"
     assert response.sms['79956896018'].status_code == 207
     assert response.balance == 4122.56
+
+
+def test_aio_send_any_sms(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(
+        url="https://sms.ru/sms/send?api_id=apfel&to[79991398805]=hello+world&to[79956896018]=hello+world&json=1",
+        json={
+            "status": "OK",
+            "status_code": 100,
+            "sms": {
+                "79991398805": {
+                    "status": "OK",
+                    "status_code": 100,
+                    "sms_id": "000000-10000000"
+                },
+                "79956896018": {
+                    "status": "ERROR",
+                    "status_code": 207,
+                    "status_text": "По небритому анусу пошло всё"
+                }
+            },
+            "balance": 4122.56
+        }
+    )
+    client = SMSru("apfel")
+    response = client.send_sms(["79991398805", "79956896018"], ['hello world', 'hello world'])
+    assert response.status_code == 100
+    assert response.status == "OK"
+    assert response.sms['79991398805'].sms_id == "000000-10000000"
+    assert response.sms['79956896018'].status_code == 207
+    assert response.balance == 4122.56
